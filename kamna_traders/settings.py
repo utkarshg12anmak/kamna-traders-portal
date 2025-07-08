67bf409ef4d15877edd29629601faf6ee94fdc49
Utkarsh Gupta <utkarsh.gupta@anmaksolar.com>
2025-07-08 15:15:18 +0530

    v0.0.12

--- kamna_traders/settings.py
+++ kamna_traders/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = "kamna_traders.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [ BASE_DIR / "Templates" ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
