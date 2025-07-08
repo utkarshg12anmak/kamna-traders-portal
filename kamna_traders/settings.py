6008852b0cf8939f1cad491eaa020223fc25446a
Utkarsh Gupta <utkarsh.gupta@anmaksolar.com>
2025-07-09 01:38:01 +0530

    v0.0.14

--- kamna_traders/settings.py
+++ kamna_traders/settings.py
@@ -122,9 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -133,5 +130,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'   # or wherever you want to land post-login
+
+STATICFILES_DIRS = [ BASE_DIR / 'static' ] 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'   
 
