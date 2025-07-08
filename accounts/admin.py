0b59bccc4c2f1c0323a71c9bf11122b9b4b41149
Utkarsh Gupta <utkarsh.gupta@anmaksolar.com>
2025-07-08 12:43:49 +0530

    v0.0.3

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+#accounts/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # Add the 'job_title' field to the existing UserAdmin fieldsets
+    fieldsets = UserAdmin.fieldsets + (
+        ("Job Info", {"fields": ("job_title",)}),
+    )
+
+    # Show 'job_title' in the user list display
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'job_title',
+        'is_active',
+        'is_staff',
+    )
