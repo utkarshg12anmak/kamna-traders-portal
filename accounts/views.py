c7d5502b51769f79d2c1d2e204601c6bef0094a1
Utkarsh Gupta <utkarsh.gupta@anmaksolar.com>
2025-07-09 02:41:44 +0530

    v0.0.19

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
+#accounts/views.py
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+@login_required
+def dashboard(request):
+    # You can pass in dynamic data here later (e.g. user role)
+    options = [
+        #{ 'label': 'Warehouses',    'url': 'warehouses:index'    },
+        # 'label': 'Sales',         'url': 'sales:index'         },
+        #{ 'label': 'Manufacturing', 'url': 'manufacturing:index' },
+        # add more as you build out apps…
+    ]
+    return render(request, 'dashboard.html', { 'options': options })
--- kamna_traders/urls.py
+++ kamna_traders/urls.py
@@ -4,12 +4,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 from django.views.generic import RedirectView
+from accounts.views import dashboard
 
 def home(request):
     return HttpResponse("Kamna Traders Portal is up and running!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', dashboard, name='dashboard'),
     path('', RedirectView.as_view(url='login/', permanent=False)),
     path('', include('accounts.urls')),
+
 ]
