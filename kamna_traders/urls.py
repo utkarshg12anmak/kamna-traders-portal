238264749af86099c2d2869902f6c7ba61af0306
Utkarsh Gupta <utkarsh.gupta@anmaksolar.com>
2025-07-09 01:58:26 +0530

    v0.0.17

--- kamna_traders/urls.py
+++ kamna_traders/urls.py
@@ -3,12 +3,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.views.generic import RedirectView
 
 def home(request):
     return HttpResponse("Kamna Traders Portal is up and running!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='login/', permanent=False)),
     path('', include('accounts.urls')),
 ]
-
