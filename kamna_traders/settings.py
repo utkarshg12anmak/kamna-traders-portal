abb71f1fa734af2ef49b018b59e5f0f1df85ae94
Utkarsh Gupta <utkarsh.gupta@anmaksolar.com>
2025-07-09 22:49:01 +0530

    v0.0.44

--- kamna_traders/settings.py
+++ kamna_traders/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'accounts',  
-    'department'  
+    'department',
 ]
 
 AUTH_USER_MODEL = "accounts.User"
